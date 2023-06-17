fa8ea61eb2664e81935967255ab3da0e4524338f
Rohit Panwar <74671410+Rohit87911@users.noreply.github.com>
2023-06-18 01:56:11 +0530

    .

--- toweerofHanoi.java
+++ toweerofHanoi.java
@@ -5,7 +5,7 @@ public class toweerofHanoi{
             return;
         }
         toh(n-1,start , mid, end);
-        System.out.println("Move Disk" + n + "from rod "+ start + " to rod " + end + "" + mid);
+        System.out.println("Move Disk" + n + "from rod "+ start + " to rod " + end);
         toh(n-1,mid,end,start);
     }
     public static void main(String[] args) {
